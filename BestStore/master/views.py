68f6f92c601d413bb0bbb6d86748458e6adc49ba
Yash Malik <yash.malik@tothenew.com>
2019-05-20 12:21:46 +0530

    login and registration optimized

--- BestStore/master/views.py
+++ BestStore/master/views.py
@@ -7,3 +7,7 @@ def home(request):
 
 def register(request):
     return render(request, "master/register.html")
+
+
+def render_login_form(request):
+    return render(request, 'master/login.html')
--- BestStore/user_master/urls.py
+++ BestStore/user_master/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import testpost
+from .views import register_user, user_login
+
 urlpatterns = [
-    path('register/', testpost, name='register_post')
+    path('register/', register_user, name='register_post'),
+    path('login/', user_login, name='user_login')
 ]
