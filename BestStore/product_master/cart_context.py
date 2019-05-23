872027dea5173460c8ca2af5c3397a521252fdc0
Aditya <aditya.arora@tothenew.com>
2019-05-23 12:40:24 +0530

    Jinja Fixes

--- BestStore/product_master/cart_context.py
+++ BestStore/product_master/cart_context.py
@@ -4,10 +4,11 @@ from .models import Product
 def add_session_cart(request):
     total_qty = 0
     total_price = 0 
-
-    for details in request.session['cart']:
-        total_qty += details['qty']
-        price = Product.objects.get(pk=details['pk']).price
-        total_price += price * details['qty']
+    cart = request.session.get('cart', False)
+    if cart:
+        for details in cart:
+            total_qty += details['qty']
+            price = Product.objects.get(pk=details['pk']).price
+            total_price += price * details['qty']
 
     return {'total_qty': total_qty, 'total_price': total_price}
